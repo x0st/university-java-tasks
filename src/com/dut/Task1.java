16a3550a20da98bfc56640602022d13db5930403
Pavel Movchan <ukrolove@gmail.com>
2018-01-15 18:37:12 +0200

    Update task1

--- src/com/dut/Task1.java
+++ src/com/dut/Task1.java
@@ -9,11 +9,14 @@ public class Task1 {
     public static void main(String[] args) {
         Text text = new Text(TextProvider.loremIpsum());
         ArrayList<Sentence> sentences = text.getSentences();
+        int i = 0;
 
         for (Sentence sentence : sentences) {
             if (Util.hasRepeatingWords(sentence)) {
-                System.out.println(sentence);
+                i++;
             }
         }
+
+        System.out.println(i);
     }
 }
